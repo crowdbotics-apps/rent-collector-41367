aee29ae3390fc8c5d2dde78add52b175cb093fc1
Crowdbotics <team@crowdbotics.com>
2023-06-08 17:47:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rent-collector-41367",
+  url: "https://rent-collector-41367.botics.co",
+  api: "https://rent-collector-41367.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
